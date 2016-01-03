df3c618a3c43029f323a99f4512ef863d10cc1eb
phpdude <alexandr.shurigin@gmail.com>
2016-01-03 02:32:21 +0200

    settings paths fixes

--- project/settings/paths.py
+++ project/settings/paths.py
@@ -1,7 +1,9 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+import sys
+
 PROJECT_DIR = os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
 BASE_DIR = os.path.realpath(os.path.dirname(PROJECT_DIR))
 
-print 'settings.BASE_DIR is "%s", settings.PROJECT_DIR is "%s"' % (BASE_DIR, PROJECT_DIR)
+sys.stderr.write('settings.BASE_DIR is "%s", settings.PROJECT_DIR is "%s"\n' % (BASE_DIR, PROJECT_DIR))
